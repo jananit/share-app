47c03716e701e0eb24dc205cc87802572fc714b8
janani <jananirails@gmail.com>
2014-02-01 22:48:43 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileSharingApp::Application.config.secret_token = '10cbb40ad66230deeb6da25d0958d3ad55a40bc8600f09b01fd51461fd697b3daca1d72c9b48a678ac7e46b1f66607c816656fe3ad413fc8247a21f65a94ce5e'
