dcdbdb12eac46369979b52741384199087a2af2b
janani <jananirails@gmail.com>
2014-02-03 01:00:39 +0530

    fifith commit

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,15 +1,12 @@
 class Project < ActiveRecord::Base
-  attr_accessible :avatar
-  # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+ attr_accessible  :avatar
+
   has_attached_file :avatar, styles: {
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
   }
-# has_attached_file :avatar,
-#   :storage => :s3, 
-#   :s3_credentials => "#{Rails.root}/config/s3.yml",
-#   :path => "project/:style/:id/:filename"
-#  validates_attachment_size :avatar, :less_than => 20.megabytes
 
+	validates_attachment_content_type :avatar, :content_type => /(image\/(jpeg|jpg|gif|png))/, :message=>"Sorry - invalid file type. Please upload a .jpg, .gif or .png"
+	  # validates_attachment :avatar, :content_type => { :content_type => "image/jpg" }
 end
